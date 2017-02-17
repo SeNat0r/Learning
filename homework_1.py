cab7857b7c7e6ac97d80303cb46eec21fa450bd0
SeNat0r <sergey1993@yandex.ru>
2017-02-17 12:54:13 +0300

    Все задачи готовы

--- homework_1.py
+++ homework_1.py
@@ -13,12 +13,35 @@ elif zadacha == 2:
     print('Введите размер участка(в сотках)')
     sotka = input()
     S = int(sotka) * 100
-    print('Введите длинну и ширину занятого пространства (м)')
-    a = int(input())
-    b = int(input())
+    print('Введите длинну и ширину занятого пространства')
+    a = int(input('Длинна (м)= '))
+    b = int(input('Ширина (м)= '))
     free = S - a * b
     if free > 0:
         print('Незанято', free, 'метров')
     else:
         print('Вы прихватизировали', -free, 'метров у соседа')
 elif zadacha == 3:
+    print('Введите координаты точки А')
+    xa = int(input('x: '))
+    ya = int(input('y: '))
+    print('Введите координаты точки B')
+    xb = int(input('x: '))
+    yb = int(input('y: '))
+    print('Введите координаты точки C')
+    xc = int(input('x: '))
+    yc = int(input('y: '))
+    if xa == xb == xc or ya == yb == yc:
+        print('Это не треугольник!')
+    elif (xa == xb and ya == yb) or (xa == xc and ya == yc) or (xc == xb and yc == yb):
+        print('Я в ответах ограничен, задавай правильные координаты...')
+    else:
+        if xa == xb or xa == xc or xb == xc:
+            if ya == yb or ya == yc or yb == yc:
+                print('Это правильный треугольник!')
+            else:
+                print('Это не правильный треугольник')
+        else:
+            print('Это не правильный треугольник')
+else:
+    print("Нет такого номера задачи!")
