29669918bb97f06f8bcd3d7da7bb93524623e46c
balabanovsb <sergey1993@yandex.ru>
2017-02-20 13:02:38 +0300

    +1

---
+++ homework_2/2.py
@@ -0,0 +1,25 @@
+lst = []
+n = 0
+print('Введите список чисел. \n Чтобы закончить ввод введите "stop"')
+while True:
+    n = input('Число: ')
+    if n != 'stop':
+        lst.append(int(n))
+    else:
+        break
+# lst = [1, 2, 3, 4, 5, 6, 7, 89, 45, 11]
+i = 0
+i2 = len(lst) - 1
+r = 0
+for x in lst:
+    r += 1
+    if r <= (len(lst) // 2):
+        lst[i] = lst[i2]
+        # print(lst[i])
+        lst[i2] = x
+        # print(lst[i2])
+        i += 1
+        i2 -= 1
+    else:
+        break
+print(lst)
