1886603a5449fe388f3d21656b9a55b4cbfec023
balabanovsb <sergey1993@yandex.ru>
2017-03-03 11:01:11 +0300

    +1

---
+++ Scheluder/scheluder/storage.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+SQL_SELECT = '''
+    SELECT
+        id, task_name, task_date, text, status
+    FROM
+        scheluder
+'''
+
+
+# Вывод значений из базы в списке (а не в кортеже)
+def dict_factory(cursor, row):
+    d = {}
+    for i, col in enumerate(cursor.description):
+        d[col[0]] = row[i]
+    return d
+
+
+# Подключение(создание) к базе данных
+def connect(db_name=None):
+    # Если название базы не указано, загружать базу в ОЗУ
+    if db_name is None:
+        db_name = ':memory:'
+
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = dict_factory
+
+# Инициализация базы
+def initialize(conn):
+    with conn:
+        cursor = conn.executescript('''
+            CREATE TABLE IF NOT EXISTS scheluder (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                task_name TEXT NOT NULL DEFAULT '',
+                task_date TEXT NOT NULL DEFAULT '',
+                text TEXT NOT NULL DEFAULT '',
+                status INTEGER NOT NULL DEFAULT 0
+            )
+        ''')
