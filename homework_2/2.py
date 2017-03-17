c60ffd139f78b78a8d9c07d1c1269077b53aff87
balabanovsb <sergey1993@yandex.ru>
2017-03-17 09:50:23 +0300

    Переделан

--- homework_2/2.py
+++ homework_2/2.py
@@ -7,19 +7,25 @@ while True:
         lst.append(int(n))
     else:
         break
+
 # lst = [1, 2, 3, 4, 5, 6, 7, 89, 45, 11]
-i = 0
-i2 = len(lst) - 1
-r = 0
-for x in lst:
-    r += 1
-    if r <= (len(lst) // 2):
-        lst[i] = lst[i2]
-        # print(lst[i])
-        lst[i2] = x
-        # print(lst[i2])
-        i += 1
-        i2 -= 1
-    else:
-        break
+# i = 0
+# i2 = len(lst) - 1
+# r = 0
+# for x in lst:
+#     r += 1
+#     if r <= (len(lst) // 2):
+#         lst[i] = lst[i2]
+#         # print(lst[i])
+#         lst[i2] = x
+#         # print(lst[i2])
+#         i += 1
+#         i2 -= 1
+#     else:
+#         break
+# print(lst)
+
+for i in range(len(lst) // 2):
+    lst[i], lst[len(lst) - 1 - i] = lst[len(lst) - 1 - i], lst[i]
+
 print(lst)
