c0f70e127d6073d8fc40a568f060e87617ae0a37
balabanovsb <sergey1993@yandex.ru>
2017-03-17 10:09:28 +0300

    Переделан

--- homework_2/4.py
+++ homework_2/4.py
@@ -1,10 +1,26 @@
-dec = int(input('Введите десятичное число: '))
-bin = ""
-while dec > 0:
-    srvs = dec / 2
-    dec = dec // 2
-    if dec == srvs:
-        bin = "0" + bin
-    else:
-         bin = "1" + bin
-print(bin)
+def bn(dec_num):
+    bin_num = ""
+    while dec_num > 0:
+        srvs = dec_num / 2
+        dec_num //= 2
+        if dec_num == srvs:
+            bin_num = "0" + bin_num
+        else:
+            bin_num = "1" + bin_num
+    return bin_num
+
+
+def dn(bin_num):
+    dec_number = 0
+    for n in range(len(bin_num)):
+        dec_number += int(bin_num[len(bin_num) - 1 - n]) * (2 ** n)
+    return dec_number
+
+
+action = int(input('1: Dec to Bin, 2: Bin to Dec\n'))
+if action == 1:
+    d = int(input('Введите десятичное число: '))
+    print(bn(d))
+elif action == 2:
+    b = input('Введите двоичное число: ')
+    print(dn(b))
