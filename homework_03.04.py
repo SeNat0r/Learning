d26d016425ea979f3b0971a17a9c1c91f0a9821c
balabanovsb <sergey1993@yandex.ru>
2017-04-06 12:55:05 +0300

    Односвязный(Готов) + двусвязный список (Готов)

--- homework_03.04.py
+++ homework_03.04.py
@@ -2,12 +2,21 @@ import copy
 
 
 class Node(object):
+    """Создание ячейки с данными и ссылкой на следующую ячейку"""
     def __init__(self, d, n=None):
         self.data = d
         self.next_node = n
 
 
 class LinkedLIst(object):
+    """
+    Односвязный список:
+    Содержит:
+        ссылку на последнюю ячейку
+        ссылку на корневую ячейку
+        размер списка
+        вспомогательюную индексацию ячейки
+    """
     def __init__(self, r=None):
         self.last = r
         self.root = None
@@ -15,12 +24,17 @@ class LinkedLIst(object):
         self.i = 0
 
     def show(self):
+        """Вывод списка"""
         node = self.root
         while node:
             print(node.data)
             node = node.next_node
 
     def __getitem__(self, item):
+        """
+        Индексация списка:
+            Итерация по списку с помощью корневой ячейки и ссылок на последующие
+        """
         i = 0
         node = self.root
         if item <= self.size:
@@ -32,6 +46,7 @@ class LinkedLIst(object):
             raise IndexError('index out of range')
 
     def __setitem__(self, key, value):
+        """Замена значений по индексу"""
         i = 0
         node = self.root
         if key <= self.size:
@@ -46,6 +61,7 @@ class LinkedLIst(object):
         return self
 
     def __next__(self):
+        """Итератор"""
         if self.i == self.size:
             raise StopIteration
         else:
@@ -54,6 +70,7 @@ class LinkedLIst(object):
             return self[i]
 
     def __contains__(self, item):
+        """Проверка вхождения значения"""
         node = self.last
         while node:
             if node.data == item:
@@ -64,6 +81,7 @@ class LinkedLIst(object):
         return self.size
 
     def __add__(self, other):
+        """Сложение списков, создающее новый список"""
         new_list = self
         for b in other:
             new_list.add(b)
@@ -75,6 +93,7 @@ class LinkedLIst(object):
     #     self.size += 1
 
     def add(self, d):
+        """Добавление новой ячейки в конец списка"""
         node = self.last
         new_node = Node(d)
         if node:
@@ -85,6 +104,7 @@ class LinkedLIst(object):
         self.size += 1
 
     def insert(self, d, i):
+        """Вставка ячейки по индексу"""
         idx = 1
         node = self.root
         if i <= self.size and i != 0:
@@ -103,6 +123,9 @@ class LinkedLIst(object):
 
 
 class DLNode(object):
+    """
+    Создание ячейки с данными и ссылкой на следующую и предыдущую ячейку
+    """
     def __init__(self, d, n=None, p=None):
         self.data = d
         self.next_node = n
@@ -110,6 +133,14 @@ class DLNode(object):
 
 
 class DoubleLinkedLIst(object):
+    """
+    Двусвязный список:
+    Содержит:
+        ссылку на последнюю ячейку
+        ссылку на корневую ячейку
+        размер списка
+        вспомогательюную индексацию ячейки
+    """
     def __init__(self, r=None):
         self.last = r
         self.root = None
@@ -117,12 +148,17 @@ class DoubleLinkedLIst(object):
         self.i = 0
 
     def show(self):
+        """Вывод списка"""
         node = self.root
         while node:
             print(node.data)
             node = node.next_node
 
     def __getitem__(self, item):
+        """
+        Индексация списка:
+            Итерация по списку с помощью корневой ячейки и ссылок на последующие
+        """
         i = 0
         node = self.root
         if item <= self.size:
@@ -134,6 +170,7 @@ class DoubleLinkedLIst(object):
             raise IndexError('index out of range')
 
     def __setitem__(self, key, value):
+        """Замена значений по индексу"""
         i = 0
         node = self.root
         if key <= self.size:
@@ -148,6 +185,7 @@ class DoubleLinkedLIst(object):
         return self
 
     def __next__(self):
+        """Итератор"""
         if self.i == self.size:
             raise StopIteration
         else:
@@ -156,6 +194,7 @@ class DoubleLinkedLIst(object):
             return self[i]
 
     def __contains__(self, item):
+        """Проверка вхождения значения"""
         node = self.last
         while node:
             if node.data == item:
@@ -166,6 +205,7 @@ class DoubleLinkedLIst(object):
         return self.size
 
     def __add__(self, other):
+        """Сложение списков, создающее новый список"""
         new_list = DoubleLinkedLIst()
         for a in self:
             new_list.add(a)
@@ -174,6 +214,7 @@ class DoubleLinkedLIst(object):
         return new_list
 
     def add(self, d):
+        """Добавление новой ячейки в конец списка"""
         node = self.last
         new_node = DLNode(d, p=node)
         if node:
@@ -183,23 +224,28 @@ class DoubleLinkedLIst(object):
             self.root = new_node
         self.size += 1
 
-    # def insert(self, d, i):
-    #     idx = 1
-    #     node = self.root
-    #     if i <= self.size and i != 0:
-    #         while idx != i:
-    #             idx += 1
-    #             node = node.next_node
-    #         new_node = DLNode(d, n=node, p=node.prev_node)
-    #         node.prev_node = new_node
-    #         self.size += 1
-    #     elif i == 0:
-    #         new_node = DLNode(d, n=self.root)
-    #         node.prev_node = new_node
-    #         self.root = new_node
-    #         self.size += 1
-    #     else:
-    #         raise IndexError('index out of range')
+    def insert(self, d, i):
+        """Вставка ячейки по индексу"""
+        idx = 1
+        node = self.root
+        # print(node)
+        if i <= self.size and i != 0:
+            while idx != i:
+                idx += 1
+                node = node.next_node
+                # print('node', node.prev_node)
+            new_node = DLNode(d, n=node.next_node, p=node)
+            # print(new_node)
+            node.next_node = new_node
+            # print(node.prev_node)
+            self.size += 1
+        elif i == 0:
+            new_node = DLNode(d, n=self.root)
+            node.prev_node = new_node
+            self.root = new_node
+            self.size += 1
+        else:
+            raise IndexError('index out of range')
 
 
 l = LinkedLIst()
@@ -222,7 +268,8 @@ t.add('4231231')
 t.add('ололо123123')
 # new = l + t
 # l.show()
-t.insert('добавление', 2)
-
-for m in t:
-    print(m)
+t.insert('добавление', 5)
+#
+# for m in t:
+#     print(m)
+t.show()
